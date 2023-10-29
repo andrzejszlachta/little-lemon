8095bde6e96d09ef4e72dc3d2f9e5e75753f08c2
Andrzej Szlachta <andrzej.sz123@gmail.com>
2023-10-29 20:24:26 +0100

    Add basic styling

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Main from "./components/Main";
-import Nav from "./components/Nav";
 
 function App() {
   return (
     <>
       <Header></Header>
-      <Nav></Nav>
       <Main></Main>
       <Footer></Footer>
     </>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,11 @@
-import logo from "../assets/logo.png";
+import logo from "../assets/logo-wide.svg";
+import Nav from "./Nav";
 
 function Header() {
   return (
     <header>
       <img src={logo} alt="little lemon logo" className="logo" />
+      <Nav />
     </header>
   );
 }
